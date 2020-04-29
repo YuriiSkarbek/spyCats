906f26cdc18b1628e5b005e28fe580e7a78f22d6
yuriiskarbek <yurii.skarbek@gmail.com>
2020-04-29 22:59:13 +0300

    Init commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;700&display=swap');
+body{
+  font-family: 'Open Sans', sans-serif;
+  background-color: rgb(44, 44, 44);
+  color: #fafafa;
+
+}
+
+
+
+
+</style>
---
+++ src/components/StartScreen.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="start">
+      <font-awesome-icon icon="user-secret" class="start-icon"/>
+      <div class="start-text">{{name}}</div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'StartScreen',
+  data () {
+    return {
+      name: 'Spy Cats'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+.start{
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+}
+
+.start-icon{
+  font-size: 154px;
+}
+
+.start-text{
+  font-size: 32px;
+  width: 100%;
+  text-align: center;
+  margin-top: 15px;
+  font-weight: 300;
+}
+</style>
